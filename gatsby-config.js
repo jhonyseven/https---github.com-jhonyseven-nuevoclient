7b84256d497dc8a9beb001f2ea8278f2793458d8
jhonalexandercadena@gmail.com <jhon@lallavedigital.com>
2020-07-16 13:07:59 +0200

    cambios ultimos

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
       }
     },
     {
-      resolve: "",//'gatsby-plugin-mdx',
+      resolve: 'gatsby-plugin-mdx',
       options: {
         defaultLayouts: {
           default: require.resolve('./src/components/Layout.js')
